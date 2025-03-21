c9b196a6385d8b6b611623878faf1fdc7d6e52ea
RaviTiwar1 <ravitiwari80085@gmail.com>
2025-03-22 04:07:24 +0530

    backtracking

--- backtracking/Rat_in_a_maze.cpp
+++ backtracking/Rat_in_a_maze.cpp
@@ -57,3 +57,29 @@ vector<string> searchMaze(vector<vector<int>> &arr, int n) {
     solve(0, 0, arr, n, ans, visited, path);
     return ans;
 }
+int main() {
+    int n;
+    cout << "Enter the size of the maze: ";
+    cin >> n;
+
+    vector<vector<int>> maze(n, vector<int>(n));
+    cout << "Enter the maze matrix (0 for blocked cell, 1 for open cell):" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> maze[i][j];
+        }
+    }
+
+    vector<string> result = searchMaze(maze, n);
+
+    if (result.empty()) {
+        cout << "No path found" << endl;
+    } else {
+        cout << "Paths found:" << endl;
+        for (const string &path : result) {
+            cout << path << endl;
+        }
+    }
+
+    return 0;
+}
